fca2de2b5bd957ba4fd1c457f946588ff87d8a18
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-07-26 09:59:41 -0600

    config dotenv

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,8 @@
 // Update with your config settings.
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 
 module.exports = {
 
@@ -6,4 +10,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/crud-snacks-review'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
 };
